6b0d75a0943b96e33fa15ef075346572d66d6d6d
itsMrGaurav <gauravkm589@gmail.com>
2025-08-10 15:12:38 +0530

    merge inetrvals

---
+++ Arrays/MergeIntervals.js
@@ -0,0 +1,45 @@
+// Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+// Example 1:
+
+// Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+// Output: [[1,6],[8,10],[15,18]]
+// Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+// Example 2:
+
+// Input: intervals = [[1,4],[4,5]]
+// Output: [[1,5]]
+// Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+// Constraints:
+
+// 1 <= intervals.length <= 104
+// intervals[i].length == 2
+// 0 <= starti <= endi <= 104
+
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function (intervals) {
+  intervals.sort((x, y) => x[0] - y[0]);
+  const merged = [intervals[0]];
+  let mCount = 0;
+  for (let i = 1; i < intervals.length; i++) {
+    if (intervals[i][0] > merged[mCount][1]) {
+      merged.push(intervals[i]);
+      mCount++;
+    } else if (
+      intervals[i][0] < merged[mCount][1] &&
+      intervals[i][1] < merged[mCount][1]
+    ) {
+      continue;
+    } else if (
+      intervals[i][0] <= merged[mCount][1] &&
+      intervals[i][1] >= merged[mCount][0]
+    ) {
+      merged.push([merged.pop()[0], intervals[i][1]]);
+    }
+  }
+  return merged;
+};
